758aca2fbe5b104e45720063c9e846a66c13e0e4
Kernael92 <kernaelapuko5@gmail.com>
2018-10-28 23:55:17 +0300

    add a new component Git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitComponent
   ],
   imports: [
     BrowserModule
