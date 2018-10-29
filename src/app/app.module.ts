8dea7566b8130b27f1bfe6fa841bb9c43b25ec0b
Kernael92 <kernaelapuko5@gmail.com>
2018-10-30 00:48:18 +0300

    install the routing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { GitService } from './git.service';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { GitComponent } from './git/git.component';
+import { SearchComponent } from './search/search.component';
+//Defining Routes
+const routes: Routes = [
+{path: "git",component:GitComponent},
+{path: "search",component:SearchComponent}
+]
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GitComponent,
+    SearchComponent
 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [GitService],
   bootstrap: [AppComponent],
